337d184655fbc3e1ba8bd41ae4992f6906fe102e
NaveenaMadesh <naveenasanthi6462@gmail.com>
2025-09-29 00:51:31 +0530

    Add files via upload

---
+++ 7Variables.py
@@ -0,0 +1,17 @@
+'''Python allows us to assign a value to multiple variables and
+multiple values to multiple variables in a single statement
+which is also known as multiple assignment.'''
+x=y=z= 50
+print (x)
+print (y)
+print (z)
+
+a,b,c = 10,20,30
+print(a)
+print(b)
+print(c)
+
+#Will learn about packing and unpacking later !!!!
+a,b,c = 10,20
+print(c)
+print(a)
