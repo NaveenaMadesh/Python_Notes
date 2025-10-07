2aa0861cdf8a5faf0bc3fe8a050e81a9129d964e
NaveenaMadesh <naveenasanthi6462@gmail.com>
2025-10-08 00:29:46 +0530

    Add files via upload

---
+++ Day10/4module.py
@@ -0,0 +1,6 @@
+#create a modeule 
+#in that ypu should have 2 functions
+#here the use from package import func_name
+#use modulename.funcname()
+# or import package
+# package_name.file_name.func_name()
