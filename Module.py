d7a15f428a97be43443cffaeb5d80f8c37f688fc
NaveenaMadesh <naveenasanthi6462@gmail.com>
2025-10-14 04:45:03 +0530

    Add files via upload

---
+++ Module.py
@@ -0,0 +1,9 @@
+# A Module --> A Python File(.py)
+# we can import module as
+# import module_nmae
+#If I want particular function
+#from module_name import particular functions
+#import with alias import calculator as calc
+# then use for each function modulename.func_name
+#from calculator import * 
+#
